e7dfa75b94a1b6c17b475c73f3bd2ee8389b4caf
PrashantKuntala <prashantkuntala@gmail.com>
2018-12-08 13:39:38 -0500

    VSCode changes

--- models/newsModel.js
+++ models/newsModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // there are many validations that mongoose handles for you, might want to read the documentation.
-// each news item contains few samples, below sample takes only one sample, you might read
-// documentation for mongoose and change it to a list
+// each news item contains few samples, below sample takes only one sample, you might want to read
+// the documentation for mongoose and change it to a list
 const newsSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     sample : {type : mongoose.Schema.Types.ObjectId, ref:'Sample' , required : true},

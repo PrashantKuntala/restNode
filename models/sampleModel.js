f5e4e1bdaf5abf49300f7f4a62868f5aedb67f5a
PrashantKuntala <prashantkuntala@gmail.com>
2018-12-08 13:42:24 -0500

    add comments

--- models/sampleModel.js
+++ models/sampleModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 // there are many validations that mongoose handles for you, might want to read the documentation.
+// Is using ORMs like mongoose always necessary ?
 const sampleSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     sampleId : {type:Number, required : true},

8bfffda040018d1aed2fb7cd009c19a72985177c
PrashantKuntala <prashantkuntala@gmail.com>
2019-02-06 10:59:28 -0500

    Removed previous cors code, this fixes any previous PATCH header setting errors

--- app.js
+++ app.js
@@ -23,17 +23,6 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/'+Config.monogodbName,{ useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
-// adding (middle-ware)response headers to handle CORS
-// app.use((req,res,next)=> {
-//     res.header('Access-Control-Allow-Origin','*');
-//     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With,Content-Type,Accept,Authorization');
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
-//         res.status(200).json({});
-//     }
-//     next();
-// });
-
 // enable cors
 app.use(cors());
 
